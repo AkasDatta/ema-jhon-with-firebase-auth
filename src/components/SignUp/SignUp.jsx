38304d15b352ee372c1f5bac99ed28b64779505c
Akas_Datta <012akasdatta@gmail.com>
2023-04-23 16:59:38 +0600

    create login and signup form

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -3,9 +3,24 @@ import "./SignUp.css";
 
 const SignUp = () => {
     return (
-        <div>
-            <h2>signup</h2>
-        </div>
+        <div className='form-container'>
+        <h2 className='form-title'>Sign up</h2>
+        <form>
+            <div className="form-control">
+                <label htmlFor="email">Email</label>
+                <input type="email" name='email' id='email' required />
+            </div>
+            <div className="form-control">
+                <label htmlFor="password">Password</label>
+                <input type="password" name='password' id='password' required />
+            </div>
+            <div className="form-control">
+                <label htmlFor="password">Confirm Password</label>
+                <input type="password" name='confirm' id='password' required />
+            </div>
+            <input className='btn-submit' type="submit" value="Login" />
+        </form>
+    </div>
     );
 };
 
