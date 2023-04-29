9019a2f803fd24d2698bb8e173537035eb5f1d46
Akas_Datta <012akasdatta@gmail.com>
2023-04-29 13:24:58 +0600

    create private route and use navigate

--- src/components/Providers/AuthProvider.jsx
+++ src/components/Providers/AuthProvider.jsx
@@ -8,6 +8,7 @@ const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true); 
 
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
@@ -26,6 +27,7 @@ const AuthProvider = ({children}) => {
     useEffect( () => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
+            setLoading(false);
         });
 
         //stop observing while unmounting
@@ -36,6 +38,7 @@ const AuthProvider = ({children}) => {
 
     const authInfo = {
         user,
+        loading,
         createUser,
         signIn,
         logOut
